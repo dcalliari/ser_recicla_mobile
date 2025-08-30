4055174aa0f017f6e3fbd71168106eda81799d5b
daniel calliari <dbcalliari@gmail.com>
2025-08-29 21:47:17 -0300

    feat: restructure navigation by replacing drawer with tab layout and adding profile screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ title: 'Modal', presentation: 'modal' }} />
       </Stack>
     </GestureHandlerRootView>
