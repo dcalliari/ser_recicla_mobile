b489ba63c550bc5aad797db9e541941fc42c21ac
daniel calliari <dbcalliari@gmail.com>
2025-09-18 00:49:18 -0300

    refactor(tabs): rename 'Institucional' tab to 'Usuários' and remove Classes component

--- app/(tabs)/institutional.tsx
+++ app/(tabs)/institutional.tsx
@@ -1,26 +1,12 @@
-import { useState, useEffect } from 'react';
-import { Dimensions, View, Text } from 'react-native';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { useEffect } from 'react';
+import { View, Text } from 'react-native';
 import { router } from 'expo-router';
 
 import Users from '~/components/institutional/users';
-import Classes from '~/components/institutional/classes';
 import { usePermissions } from '~/lib/hooks/usePermissions';
-
-const renderScene = SceneMap({
-  first: Classes,
-  second: Users,
-});
-
-const initialLayout = { width: Dimensions.get('window').width };
 
 export default function Institutional() {
   const { isStudent, canAccessInstitutional } = usePermissions();
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'first', title: 'Turmas' },
-    { key: 'second', title: 'Usuários' },
-  ]);
 
   useEffect(() => {
     // Redireciona alunos para a home page
@@ -40,22 +26,7 @@ export default function Institutional() {
 
   return (
     <>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={initialLayout}
-        renderTabBar={(props) => (
-          <TabBar
-            {...props}
-            indicatorStyle={{ backgroundColor: 'green' }}
-            style={{ backgroundColor: 'white' }}
-            activeColor="green"
-            inactiveColor="gray"
-            tabStyle={{ height: 40 }}
-          />
-        )}
-      />
+      <Users />
     </>
   );
 }
--- components/institutional/classes.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Classes() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-lg">Turmas</Text>
-    </View>
-  );
-}
